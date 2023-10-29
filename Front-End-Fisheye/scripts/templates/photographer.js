13f821708746ca28d6661c837f1c9039e09d72d2
hzed <hzedek59@gmail.com>
2023-10-29 19:06:08 +0100

    photographerHeader + IMG
    creation de la carte individuelle des photographe pour leur page et ajout au projet de leurs photos

--- Front-End-Fisheye/scripts/templates/photographer.js
+++ Front-End-Fisheye/scripts/templates/photographer.js
@@ -1,6 +1,6 @@
 
 function photographerTemplate(data) {
-    const { name, portrait, tagline, city, price,id } = data;
+    const { name, portrait, tagline, city,country, price,id } = data;
     const picture = `assets/photographers/${portrait}`;
     function getUserCardDOM() { 
         const article = document.createElement( 'article' );
@@ -8,16 +8,20 @@ function photographerTemplate(data) {
         //Boutton cliquable sur l'image du photographe envoyant à son profil
         img.addEventListener('click', ()=>{
             document.location.href = 'http://127.0.0.1:5501/Front-End-Fisheye/photographer.html?'+id;
-        }) 
-        img.setAttribute("src", picture)
+        })
+        img.setAttribute("src", picture);
+        img.classList.add("img");
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        h2.classList.add("name")
         const p_tagline = document.createElement( 'p' );
         p_tagline.textContent = tagline;
         const p_city = document.createElement( 'p' );
-        p_city.textContent = city;
+        p_city.textContent = city+" "+country;
+        p_city.classList.add("city")
         const p_price = document.createElement( 'p' );
         p_price.textContent = price+"/jour";
+        p_price.classList.add("tagline");
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(p_city);
@@ -26,28 +30,8 @@ function photographerTemplate(data) {
         return (article);
     }
     function photographerPageDisplay() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        //Boutton cliquable sur l'image du photographe envoyant à son profil
-        img.addEventListener('click', ()=>{
-            document.location.href = 'http://127.0.0.1:5501/Front-End-Fisheye/photographer.html?'+id;
-        }) 
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        const p_tagline = document.createElement( 'p' );
-        p_tagline.textContent = tagline;
-        const p_city = document.createElement( 'p' );
-        p_city.textContent = city;
-        const p_price = document.createElement( 'p' );
-        p_price.textContent = price+"/jour";
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(p_city);
-        article.appendChild(p_tagline);
-        article.appendChild(p_price);
-        return (article);
+        
     }
     
-    return { name, picture, tagline, city, price,id, getUserCardDOM,photographerPageDisplay }
+    return { name, picture, tagline, city,country, price,id, getUserCardDOM,photographerPageDisplay }
 }
